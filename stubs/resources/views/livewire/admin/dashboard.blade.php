b5c6bbd652740a68941fc8be884e2a0653a12757
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-11-18 19:44:01 +0700

    source code landing page docmed

--- stubs/resources/views/livewire/admin/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    @section('title', __('Dashboard'))
-
-    <h1>{{ __('Dashboard') }}</h1>
-
-    <div class="card">
-        {{ __("You're logged in!") }}
-    </div>
-</div>
--- stubs/tests/Unit/Commands/MakeDatabaseCommandTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-test('can run db:build', function () {
-    $this->artisan('db:build')
-        ->expectsOutput('This command is disabled on production.');
-});
